94330498e62ca4b3ed498ef0534a62d828f4b4e6
Faaruque <faaruque99@gmail.con>
2022-03-17 23:46:37 +0600

    owl-carousel

--- script/index.js
+++ script/index.js
@@ -1,24 +1,40 @@
-// smoothscroll js Not
-var scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 100,
-  speedAsDuration: true,
-});
-
-// bottom to top scroll javaScript here
-const scrollTop = document.getElementById("scrollTop");
-window.addEventListener("scroll", scrollFuncton);
-function scrollFuncton() {
-  if (window.pageYOffset > 300) {
-    scrollTop.style.display = "block";
-  } else {
-    scrollTop.style.display = "none";
-  }
-}
-scrollTop.addEventListener("click", function () {
-  window.scrollTo(0, 0);
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
+// init owl-carousel-product-area
+$(".card-img").owlCarousel({
+  items: 1,
+  autoplay: true,
+  dots: false,
+  nav: false,
+  autoplayTimeout: 2000,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+      loop: true,
+    },
+    600: {
+      items: 1,
+      nav: true,
+      loop: true,
+    },
+    750: {
+      items: 2,
+      nav: true,
+      loop: true,
+    },
+    1024: {
+      items: 2,
+      nav: true,
+      loop: true,
+    },
+    1200: {
+      items: 3,
+      nav: true,
+      loop: true,
+    },
+    1440: {
+      items: 3,
+      nav: true,
+      loop: true,
+    },
+  },
 });

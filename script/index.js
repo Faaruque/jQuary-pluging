e324cb4e97a06e2e2083cb4486e722194aafb052
Faaruque <faaruque99@gmail.con>
2022-03-29 16:10:00 +0600

    owl-carousel-pluging

--- script/index.js
+++ script/index.js
@@ -1,40 +1,24 @@
-// init owl-carousel-product-area
-$(".card-img").owlCarousel({
-  items: 1,
-  autoplay: true,
-  dots: false,
-  nav: false,
-  autoplayTimeout: 2000,
-  responsive: {
-    0: {
-      items: 1,
-      nav: true,
-      loop: true,
+$(document).ready(function () {
+  // owl carousel script
+  $(".teams-wapper").owlCarousel({
+    margin: 20,
+    autoplay: true,
+    loop: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
     },
-    600: {
-      items: 1,
-      nav: true,
-      loop: true,
-    },
-    750: {
-      items: 2,
-      nav: true,
-      loop: true,
-    },
-    1024: {
-      items: 2,
-      nav: true,
-      loop: true,
-    },
-    1200: {
-      items: 3,
-      nav: true,
-      loop: true,
-    },
-    1440: {
-      items: 3,
-      nav: true,
-      loop: true,
-    },
-  },
+  });
 });

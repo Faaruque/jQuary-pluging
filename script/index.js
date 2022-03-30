e515956ac93a1a8e012ba7c30fc2e0c8d1778ce2
Faaruque <faaruque99@gmail.con>
2022-03-30 15:45:45 +0600

    jQuary-type-script

---
+++ script/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Web Developer", "Web Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+});
